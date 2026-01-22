2987d02111c8c65af1dec06e9087fb6c53b1e07a
Danielber2002 <danielber2002@gmail.com>
2026-01-22 23:46:13 +0100

    Autoescritura

---
+++ Scripts/Lore.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class Lore : MonoBehaviour
+{
+    [Header("Configuración")]
+    public float tiempoEntreLetras = 0.05f;
+    public string contenidoDelTexto;
+
+    private TextMeshProUGUI miTextoUI;
+
+    void Awake()
+    {
+        miTextoUI = GetComponent<TextMeshProUGUI>();
+    }
+
+
+    void Start()
+    {
+
+        if (string.IsNullOrEmpty(contenidoDelTexto))
+        {
+            contenidoDelTexto = miTextoUI.text;
+        }
+
+        StartCoroutine(EscribirTexto());
+    }
+    void Update()
+    {
+        MoreVelocity();
+    }
+
+    void MoreVelocity()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            tiempoEntreLetras = 0.001f;
+        }
+    }
+
+    IEnumerator EscribirTexto()
+    {
+        miTextoUI.text = ""; 
+
+        foreach (char letra in contenidoDelTexto.ToCharArray())
+        {
+            miTextoUI.text += letra;
+            yield return new WaitForSeconds(tiempoEntreLetras);
+        }
+    }
+}
