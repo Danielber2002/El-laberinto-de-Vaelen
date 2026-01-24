cdb24c4bfafd44a60b1c13cced4c1d5e78d2931d
Danielber2002 <danielber2002@gmail.com>
2026-01-24 03:24:26 +0100

    Music

---
+++ Scenes/SorremScenes/AdvanceScripts/Sorrem0Introduccion.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Sorrem0Introduccion : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CambiarEscena()
+    {
+        SceneManager.LoadScene("Sorrem-0 Introduccion");
+    }
+}
