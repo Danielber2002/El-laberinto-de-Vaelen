36dfa8d214e67b24a8efe12001c7c4009d9423d0
Javier Ríos Gabella <jriosgabella@gmail.com>
2026-01-24 20:57:55 +0100

    - TitleScene: Canvas y RawImage adaptado a las difernetes resoluciones de pantalla. - Mejora de scripts de menu. - Video de inicio de la escena InitScene. - InitScene: agregar video a traves de Video Player.

--- Scripts/MenuScripts/ImageQualityOptions.cs
+++ Scripts/MenuScripts/ImageQualityOptions.cs
@@ -1,25 +1,46 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class ImageQualityOptions : MonoBehaviour
 {
-    private const string _key = "quality";
-    public TMP_Dropdown dropdown;
-    public int quality;
+    public TMP_Dropdown dropdownCalidad;
+    private const string PrefCalidad = "NivelCalidad";
 
     void Start()
     {
-        quality = PlayerPrefs.GetInt(_key, 3);
-        dropdown.value = quality;
-        SetQuality();
+        // 1. Configurar el Dropdown con los nombres de QualitySettings
+        dropdownCalidad.ClearOptions();
+        List<string> opciones = new List<string>(QualitySettings.names);
+        dropdownCalidad.AddOptions(opciones);
+
+        // 2. Cargar la calidad guardada o usar la actual por defecto
+        // Si no existe la clave "NivelCalidad", usa el nivel actual
+        int calidadGuardada = PlayerPrefs.GetInt(PrefCalidad, QualitySettings.GetQualityLevel());
+
+        // 3. Aplicar la calidad al juego y al Dropdown
+        ActualizarCalidad(calidadGuardada);
+        dropdownCalidad.value = calidadGuardada;
+        dropdownCalidad.RefreshShownValue();
+
+        // 4. Asignar el evento de cambio
+        dropdownCalidad.onValueChanged.AddListener(CambiarCalidad);
     }
 
-    public void SetQuality()
+    public void CambiarCalidad(int indice)
     {
-        QualitySettings.SetQualityLevel(dropdown.value);
-        PlayerPrefs.SetInt(_key, dropdown.value);
-        quality = dropdown.value;
+        ActualizarCalidad(indice);
+
+        // Guardar la elección permanentemente
+        PlayerPrefs.SetInt(PrefCalidad, indice);
+        PlayerPrefs.Save(); // Asegura que se escriba en el disco
+
+        Debug.Log("Calidad guardada: " + QualitySettings.names[indice]);
+    }
+
+    private void ActualizarCalidad(int indice)
+    {
+        // Cambia el nivel de calidad del motor
+        QualitySettings.SetQualityLevel(indice, true);
     }
 }
--- Scripts/MenuScripts/ResolutionOptions.cs
+++ Scripts/MenuScripts/ResolutionOptions.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class ResolutionOptions : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_Dropdown dropdownResolucion;
+    private List<Resolution> resolucionesFiltradas; // Lista limpia para usar en el juego
+    private const string PrefFullscreen = "PantallaCompleta";
+
     void Start()
     {
-        
+        ConfigurarResolucion();
     }
 
-    // Update is called once per frame
-    void Update()
+    void ConfigurarResolucion()
     {
-        
+        dropdownResolucion.ClearOptions();
+
+        // 1. Obtener todas las resoluciones
+        Resolution[] todasLasResoluciones = Screen.resolutions;
+
+        // 2. Filtrar usando LINQ:
+        // - Agrupamos por ancho y alto.
+        // - De cada grupo, seleccionamos la que tenga la mayor tasa de refresco.
+        // - Ordenamos de menor a mayor para el menú.
+        resolucionesFiltradas = todasLasResoluciones
+            .GroupBy(res => new { res.width, res.height })
+            .Select(grupo => grupo.OrderByDescending(res => res.refreshRateRatio.value).First())
+            .OrderBy(res => res.width)
+            .ThenBy(res => res.height)
+            .ToList();
+
+        List<string> opciones = new List<string>();
+        int indiceResolucionActual = 0;
+
+        for (int i = 0; i < resolucionesFiltradas.Count; i++)
+        {
+            string opcion = resolucionesFiltradas[i].width + " x " + resolucionesFiltradas[i].height;
+            opciones.Add(opcion);
+
+            // Comprobar si es la resolución que el monitor tiene activa ahora
+            if (resolucionesFiltradas[i].width == Screen.currentResolution.width &&
+                resolucionesFiltradas[i].height == Screen.currentResolution.height)
+            {
+                indiceResolucionActual = i;
+            }
+        }
+
+        dropdownResolucion.AddOptions(opciones);
+        dropdownResolucion.value = indiceResolucionActual;
+        dropdownResolucion.RefreshShownValue();
+
+        dropdownResolucion.onValueChanged.AddListener(CambiarResolucion);
+    }
+
+    public void CambiarResolucion(int indice)
+    {
+        Resolution res = resolucionesFiltradas[indice];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+    }
+
+    public void CambiarPantallaCompleta(bool estado)
+    {
+        Screen.fullScreen = estado;
+        PlayerPrefs.SetInt(PrefFullscreen, estado ? 1 : 0);
+        PlayerPrefs.Save();
     }
 }
