c6bb41115657863d1ad81d8c07e10e5b4f69e4ea
Danielber2002 <danielber2002@gmail.com>
2026-01-28 20:54:14 +0100

    Game Over

---
+++ Scripts/Dani`s Scripts/GameOver.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene("CharacterSelection");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
