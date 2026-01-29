6ee061259db4be75be1eadcb03a9aee8d13ade82
María Cebriá Galera <m.cebriagalera.mcg@gmail.com>
2026-01-29 23:58:38 +0100

    lysandra

---
+++ Scripts/Dani`s Scripts/Playerhealth1.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth1 : MonoBehaviour
+{
+    [Header("Corazones")]
+    public GameObject[] corazones;
+
+    public static int vidasRestantes = 4;
+
+    void Start()
+    {
+        ActualizarCorazonesVisuales();
+    }
+
+    public void RestarVida()
+    {
+        if (vidasRestantes > 0)
+        {
+            vidasRestantes--;
+            ActualizarCorazonesVisuales(); 
+
+            if (vidasRestantes <= 0)
+            {
+                GameOver();
+            }
+        }
+    }
+
+    void ActualizarCorazonesVisuales()
+    {
+        for (int i = 0; i < corazones.Length; i++)
+        {
+            if (i < vidasRestantes)
+            {
+                corazones[i].SetActive(true);
+            }
+            else
+            {
+                corazones[i].SetActive(false);
+            }
+        }
+    }
+
+    public void ReiniciarVidas()
+    {
+        vidasRestantes = 4;
+        ActualizarCorazonesVisuales();
+    }
+
+    void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+}
